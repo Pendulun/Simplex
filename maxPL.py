06d30bf79b8bb28b4b9d8d432c4515e812c30a15
Pendulun <danielcampos14@hotmail.com.br>
2021-07-13 15:22:05 -0300

    Criando classe PL

---
+++ maxPL.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class PL():
+    def __init__(self,c,b,restricoes):
+        self.c = c
+        self.b = b
+        self.restricoes = restricoes
+    
+    def numRestricoes(self):
+        return self.restricoes.shape[0]
+    
+    def print(self):
+        print("C: {}",format(self.c))
+        print("B: {}".format(self.b))
+        print("Restrições:\n {}".format(self.restricoes))
