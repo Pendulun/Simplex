50e0d93facef6b9f3d93b09dc01fdccc2859c84f
Pendulun <danielcampos14@hotmail.com.br>
2021-07-12 22:27:27 -0300

    Leitura de arquivo padrão

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
 import numpy as np
 
-a = np.ones(2)
-print(a)
+def leArquivo(arquivo="standard_input.txt"):
+    file = open(arquivo,"r")
+    n, m = file.readline().split()
+
+    n = int(n)
+    m = int(m)
+    linhas = []
+    c = file.readline().split()
+
+    for i in range(n):
+        linhas.append(list(file.readline().split()))
+        
+    file.close()
+    return n,m,c,linhas
+
+def main():
+    arquivo = "standard_input.txt"
+    try:
+        n,m,c,linhas=leArquivo(arquivo)
+        print(n)
+        print(m)
+        print(c)
+        print(linhas)
+    except:
+        print("Não foi possível abrir arquivo no endereço {}".format(arquivo))
+    
+if __name__ == '__main__':
+    main()
