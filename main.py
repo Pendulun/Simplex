a802390647b0dbae61896cedaa4e7a197dfe524d
Pendulun <danielcampos14@hotmail.com.br>
2021-07-12 23:13:49 -0300

    Usando numpy arrays

--- main.py
+++ main.py
@@ -6,28 +6,42 @@ def leArquivo(arquivo="standard_input.txt"):
 
     n = int(n)
     m = int(m)
-    restricoes = []
-    b = []
-    c = file.readline().split()
-
+    restricoes = np.zeros((n,m), dtype="int32")
+    b = np.zeros(n, dtype="int32")
+    c = np.array(file.readline().split(), dtype="int32")
+    
     for i in range(n):
-        linha = list(file.readline().split())
-        restricoes.append(linha[:-1])
-        b.append(linha[-1])
+        linha = np.array(list(file.readline().split()))
+        restricoes[i] = linha[:-1]
+        b[i] = linha[-1]
         
     file.close()
     return n,m,c,b,restricoes
 
+def imprimeTudo(n,m,c,b,restricoes):
+    print("Número de restrições: {}".format(n))
+    print("Número de variáveis: {}".format(m))
+    print("Vetor C: {}".format(c))
+    print("Vetor B: {}".format(b))
+    print("Restrições: {}".format(restricoes))
+
+
+def trocaSinalRestricaoSeBNaoPositivo(b,restricoes):
+    print("TROCA SINAL")
+    for i in range(b.size):
+        if b[i]<0:
+            restricoes[i] = restricoes[i] * -1
+            b[i] = b[i] * -1
+
 def main():
     arquivo = "standard_input.txt"
+    
     try:
         n,m,c,b,restricoes=leArquivo(arquivo)
-        print("Número de restrições: {}".format(n))
-        print("Número de variáveis: {}".format(m))
-        print("Vetor C: {}".format(c))
-        print("Vetor B: {}".format(b))
-        print("Restrições: {}".format(restricoes))
-    except:
+        imprimeTudo(n,m,c,b,restricoes)
+        trocaSinalRestricaoSeBNaoPositivo(b,restricoes)
+        imprimeTudo(n,m,c,b,restricoes)
+    except OSError:
         print("Não foi possível abrir arquivo no endereço {}".format(arquivo))
     
 if __name__ == '__main__':
