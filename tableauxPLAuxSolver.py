4a6cb56f3ebcd90e60039735bfbe03334214f4f0
Pendulun <danielcampos14@hotmail.com.br>
2021-07-26 20:43:59 -0300

    Adicionando parte da resolução do Tableaux da PLAux

--- tableauxPLAuxSolver.py
+++ tableauxPLAuxSolver.py
@@ -1,6 +1,7 @@
 from tableauxSolver import TableauxSolver
 from tableauxAux import TableauxAux
 import math
+import numpy as np
 
 class TableauxPLAuxSolver(TableauxSolver):
 
@@ -8,9 +9,47 @@ class TableauxPLAuxSolver(TableauxSolver):
 
     def __init__(self, pl, cOriginal):
         super().__init__(pl)
-        self.__tableaux = TableauxAux(pl, cOriginal)
+        self._tableaux = TableauxAux(pl, cOriginal)
     
     def getTableaux(self):
-        return self.__tableaux
+        return self._tableaux
+
+    def resolver(self):
+        #Tratar B negativos
+        self._trataBNegativoPL()
+
+        #Adicionar Variáveis artificiais em A
+        self._adicionarVariaveisArtificiais()
+
+        #resolve
+
+        #retorna o tableaux em sua forma final
+        return self._tableaux
+
+    def _trataBNegativoPL(self):
+        print("TRATANDO B_i's negativos")
+        b = self._tableaux.getB()
+        restricoes = self._tableaux.getMatrizA()
+        tranformacoes = self._tableaux.getMatrizTransformacoes()
+        for i in range(self._tableaux.numRestricoes()):
+            if math.isclose(b[i], 0, abs_tol=self.PRECISAO):
+                #Define como 0.0, já que é perto mesmo
+                self._tableaux.attValorB(i, 0.0)
+            elif b[i] < 0:
+                # Multiplica b[i], restricoes[i] e tranformacoes[i] por -1
+                self._tableaux.attValorB(i,b[i]*-1)
+                self._tableaux.attLinhaA(i,restricoes[i]*-1)
+                self._tableaux.attLinhaMatrizTransformacoes(i,tranformacoes[i]*-1)
+        self._tableaux.print()
+    
+    def _adicionarVariaveisArtificiais(self):
+        print("ADICIONANDO VARIAVEIS ARTIFICIAIS MATRIZ A")
+        variaveisArt = self.__geraMatrizIdentidade(self._tableaux.numRestricoes())
+        self._tableaux.addMatrizA(variaveisArt)
+        self._tableaux.print()
+    
+    def __geraMatrizIdentidade(self, tam):
+        print("Gera Matriz Identidade")
+        return np.identity(tam)
 
     
