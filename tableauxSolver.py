d3e21d51411582f4a2ebbd79db02cc0a2abbc335
Pendulun <danielcampos14@hotmail.com.br>
2021-07-26 20:38:48 -0300

    Alterando nomes de variáveis

--- tableauxSolver.py
+++ tableauxSolver.py
@@ -8,31 +8,31 @@ class TableauxSolver():
     PRECISAO = 0.0001
 
     def __init__(self,pl):
-        self.__tableaux = Tableaux(pl)
-        self.__solucaoViavel = np.array(range(pl.numVariaveisRestricoes()))
-        self.__isViavel = True
-        self.__isIlimitada = True
-        self.__isOtimo = True
-        self.__certificadoIlimitada = np.array(range(pl.numRestricoes()))
+        self._tableaux = Tableaux(pl)
+        self._solucaoViavel = np.array(range(pl.numVariaveisRestricoes()))
+        self._isViavel = True
+        self._isIlimitada = True
+        self._isOtimo = True
+        self._certificadoIlimitada = np.array(range(pl.numRestricoes()))
 
     def imprimirTudo(self):
         print("Meu Tableaux")
-        print("É Viável: {}".format(self.__isViavel))
-        print("É Ilimitada: {}".format(self.__isIlimitada))
-        print("É Ótima: {}".format(self.__isOtimo))
-        self.__tableaux.print()
-        print("Solução Viável: {}".format(self.__solucaoViavel))
-        print("Certificado Ilimitada: {}".format(self.__certificadoIlimitada))
+        print("É Viável: {}".format(self._isViavel))
+        print("É Ilimitada: {}".format(self._isIlimitada))
+        print("É Ótima: {}".format(self._isOtimo))
+        self._tableaux.print()
+        print("Solução Viável: {}".format(self._solucaoViavel))
+        print("Certificado Ilimitada: {}".format(self._certificadoIlimitada))
         
     def resolver(self):
         print("TABLEAUX INICIAL:")
-        self.__tableaux.print()
+        self._tableaux.print()
 
         #enquanto houver c[i] negativo ou b[i] negativo
         while True:
             pivoteou = False
 
-            pivoteou = self.__verificarB()
+            pivoteou = self._verificarB()
 
             """
             if not pivoteou:
@@ -43,107 +43,107 @@ class TableauxSolver():
 
             #Não sei se isso está certo
             if not pivoteou:
-                self.__isOtimo = True
-                self.__isIlimitada = False
-                self.__isViavel = True
+                self._isOtimo = True
+                self._isIlimitada = False
+                self._isViavel = True
                 break
 
-    def __verificarB(self):
+    def _verificarB(self):
         pivoteou = False
         while True:
-            indexBINegativo = self.__getIndexBINegativo()
+            indexBINegativo = self._getIndexBINegativo()
             if indexBINegativo < 0:
                 break
             else:
                 print("INDEX B NEGATIVO: {}".format(indexBINegativo))
-                self.__trataBiNegativo(indexBINegativo)
+                self._trataBiNegativo(indexBINegativo)
                 pivoteou = True
         return pivoteou
     
-    def __getIndexBINegativo(self):
-        b = self.__tableaux.getB()
-        for i in range(self.__tableaux.numRestricoes()):
+    def _getIndexBINegativo(self):
+        b = self._tableaux.getB()
+        for i in range(self._tableaux.numRestricoes()):
             if math.isclose(b[i], 0, abs_tol=self.PRECISAO):
                 #Define como 0.0, já que é perto mesmo
-                self.__tableaux.attValorB(i, 0.0)
+                self._tableaux.attValorB(i, 0.0)
             elif b[i] < 0:
                 return i
         return -1
 
-    def __trataBiNegativo(self, index):
-        self.__multiplicaLinhaPor(index+1,-1)
+    def _trataBiNegativo(self, index):
+        self._multiplicaLinhaPor(index+1,-1)
         #pivotear elemento
-        self.__tableaux.print()
+        self._tableaux.print()
     
-    def __verificarC(self):
+    def _verificarC(self):
         pivoteou = False
         while True:
-            indexCINegativo = self.__getIndexCINegativo()
+            indexCINegativo = self._getIndexCINegativo()
             if indexCINegativo < 0:
                 break
             else:
                 print("INDEX C NEGATIVO: {}".format(indexCINegativo))
-                self.__trataCiNegativo(indexCINegativo)
+                self._trataCiNegativo(indexCINegativo)
                 pivoteou = True
 
         return pivoteou
     
-    def __getIndexCINegativo(self):
-        c = self.__tableaux.getC()
-        for i in range(self.__tableaux.numVariaveisC()):
+    def _getIndexCINegativo(self):
+        c = self._tableaux.getC()
+        for i in range(self._tableaux.numVariaveisC()):
             if math.isclose(c[i], 0, abs_tol=self.PRECISAO):
                 #Define como 0.0, já que é perto mesmo
-                self.__tableaux.attValorC(i, 0.0)
+                self._tableaux.attValorC(i, 0.0)
             elif c[i] < 0:
                 return i
         return -1
 
-    def __trataCiNegativo(self,index):
+    def _trataCiNegativo(self,index):
         #pivotear algum elemento
         pass
 
-    def __multiplicaLinhaPor(self,numLinhaTableaux, valor):
+    def _multiplicaLinhaPor(self,numLinhaTableaux, valor):
         if numLinhaTableaux == 0:
-            self.__multiplicaPrimeiraLinhaPor(valor)
+            self._multiplicaPrimeiraLinhaPor(valor)
         else:
-            self.__multiplicaLinhaRestoTableauxPor(numLinhaTableaux, valor)
+            self._multiplicaLinhaRestoTableauxPor(numLinhaTableaux, valor)
     
-    def __multiplicaPrimeiraLinhaPor(self,valor):
+    def _multiplicaPrimeiraLinhaPor(self,valor):
         pass
 
-    def __multiplicaLinhaRestoTableauxPor(self,numLinha, valor):
-        self.__tableaux.attLinhaMatrizTransformacoes(numLinha-1,self.__tableaux.getMatrizTransformacoes()[numLinha-1]*valor)
-        self.__tableaux.attLinhaA(numLinha-1, self.__tableaux.getMatrizA()[numLinha-1]*valor)
-        self.__tableaux.attValorB(numLinha-1,self.__tableaux.getB()[numLinha-1]*valor)
+    def _multiplicaLinhaRestoTableauxPor(self,numLinha, valor):
+        self._tableaux.attLinhaMatrizTransformacoes(numLinha-1,self._tableaux.getMatrizTransformacoes()[numLinha-1]*valor)
+        self._tableaux.attLinhaA(numLinha-1, self._tableaux.getMatrizA()[numLinha-1]*valor)
+        self._tableaux.attValorB(numLinha-1,self._tableaux.getB()[numLinha-1]*valor)
 
     def getSolucaoViavel(self):
-        if(self.__isViavel):
-            return self.__solucaoViavel.copy()
+        if(self._isViavel):
+            return self._solucaoViavel.copy()
     
     def getCertificadoOtimalidade(self):
-        if self.__isOtimo:
-            return self.__tableaux.getCertificadoOtimo()
+        if self._isOtimo:
+            return self._tableaux.getCertificadoOtimo()
     
     def getValorOtimo(self):
-        if(self.__isViavel):
-            return self.__tableaux.getValorOtimo()
+        if(self._isViavel):
+            return self._tableaux.getValorOtimo()
     
     def isViavel(self):
-        return self.__isViavel
+        return self._isViavel
     
     def isIlimitada(self):
-        return self.__isIlimitada
+        return self._isIlimitada
     
     def isOtima(self):
-        return self.__isOtimo
+        return self._isOtimo
 
     def getCertificadoIlimitada(self):
-        if self.__isIlimitada:
-            return self.__certificadoIlimitada.copy()
+        if self._isIlimitada:
+            return self._certificadoIlimitada.copy()
     
     def getCertificadoOtima(self):
-        if self.__isOtimo:
-            return self.__certificadoOtimo.copy()
+        if self._isOtimo:
+            return self._certificadoOtimo.copy()
     
     def getTableaux(self):
-        return self.__tableaux
+        return self._tableaux
