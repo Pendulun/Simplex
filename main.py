4ab7c74ed8975ddaaaeb78be51126a4a70a30210
Pendulun <danielcampos14@hotmail.com.br>
2021-07-28 21:49:22 -0300

    Alterando para ler entrada do terminal
    Agora, não é mais possível ler de um arquivo

--- main.py
+++ main.py
@@ -1,34 +1,28 @@
 import numpy as np
 from simplex import Simplex
 
-def leArquivo(arquivo="standard_input2.txt"):
-    file = open(arquivo,"r")
-    n, m = file.readline().split()
+def leEntrada():
+    n, m = input().split()
 
     n = int(n)
     m = int(m)
     restricoes = np.zeros((n,m), dtype="float32")
     b = np.zeros(n, dtype="float32")
-    c = np.array(file.readline().split(), dtype="float32")
+    c = np.array(input().split(), dtype="float32")
 
     for i in range(n):
-        linha = np.array(list(file.readline().split()))
+        linha = np.array(list(input().split()))
         restricoes[i] = linha[:-1]
         b[i] = linha[-1]
-        
-    file.close()
+
     return c,b,restricoes
 
 def main():
-    arquivo = "testes/standard_input3.txt"
     
-    try:
-        c,b,restricoes=leArquivo(arquivo)
-        my_simplex = Simplex(c,b,restricoes)
-        my_simplex.resolver()
-        my_simplex.imprimeResultado()
-    except OSError:
-        print("Não foi possível abrir arquivo no endereço {}".format(arquivo))
+    c,b,restricoes=leEntrada(arquivo)
+    my_simplex = Simplex(c,b,restricoes)
+    my_simplex.resolver()
+    my_simplex.imprimeResultado()
     
 if __name__ == '__main__':
     main()
